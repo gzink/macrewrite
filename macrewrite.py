4524a6e2373dfd5a9fc8b424d0ba33cb3590f473
Gabriel Zink <info@gabrielzink.de>
2015-07-30 18:03:35 +0200

    Update macrewrite.py

--- macrewrite.py
+++ macrewrite.py
@@ -5,11 +5,13 @@ own_mac = open('/sys/class/net/%s/address' % "eth0").read().strip()
 def handleMac(mac, ips):
     commands = []
     commands.append("ebtables -t nat -A POSTROUTING -s %s -j snat --to-src %s --snat-arp --snat-target ACCEPT" % (mac, own_mac))
-    for ip in ips["ip4"]:
-        commands.append("ebtables -t nat -A PREROUTING -p IPv4 --ip-dst %s -j dnat --to-dst %s --dnat-target ACCEPT" % (ip, mac))
-        commands.append("ebtables -t nat -A PREROUTING -p ARP --arp-ip-dst %s -j dnat --to-dst %s --dnat-target ACCEPT" % (ip, mac))
-    for ip in ips["ip6"]:
-        commands.append("ebtables -t nat -A PREROUTING -p IPv6 --ip6-dst %s -j dnat --to-dst %s --dnat-target ACCEPT" % (ip, mac))
+    if "ip4" in ips:
+        for ip in ips["ip4"]:
+            commands.append("ebtables -t nat -A PREROUTING -p IPv4 --ip-dst %s -j dnat --to-dst %s --dnat-target ACCEPT" % (ip, mac))
+            commands.append("ebtables -t nat -A PREROUTING -p ARP --arp-ip-dst %s -j dnat --to-dst %s --dnat-target ACCEPT" % (ip, mac))
+    if "ip6" in ips:
+        for ip in ips["ip6"]:
+            commands.append("ebtables -t nat -A PREROUTING -p IPv6 --ip6-dst %s -j dnat --to-dst %s --dnat-target ACCEPT" % (ip, mac))
     return commands
 
 dirname, filename = os.path.split(os.path.abspath(__file__))
